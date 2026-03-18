30c68f9fb5f72bbd8f07ea88576f9aca8f815f9a
Тимофей <titr@kode.ru>
2026-03-18 18:53:19 +0200

    well

--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const postbackInfoDiv = document.getElementById('postback-info');
+    const BACKEND_URL = 'https://testpostback-timofey3498.amvera.io';
 
     async function fetchPostbackData() {
         try {
-            const response = await fetch('/view'); 
+            const response = await fetch(`${BACKEND_URL}/view`); 
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
             const htmlContent = await response.text();
             
-            postbackInfoDiv.innerHTML = htmlContent;
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(htmlContent, 'text/html');
+            const container = doc.querySelector('.container');
+            
+            if (container) {
+                postbackInfoDiv.innerHTML = container.innerHTML;
+            } else {
+                postbackInfoDiv.innerHTML = htmlContent;
+            }
 
         } catch (error) {
             console.error('Error fetching postback data:', error);
@@ -18,4 +27,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     fetchPostbackData();
+    // Обновляем данные каждые 5 секунд
+    setInterval(fetchPostbackData, 5000);
 });
